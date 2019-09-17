41834cd2b3e2ed38f3abc9c789ffe1ee4856216b
Philipp Ensinger <philipp.ensinger@r-software.at>
2019-09-17 13:39:39 +0200

    Testing Proxy Settings

--- src/ReqReTest.java
+++ src/ReqReTest.java
@@ -1,6 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URLConnection;
+import java.util.Base64;
+import java.util.Scanner;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
@@ -14,11 +20,19 @@ public class ReqReTest {
 
     public static void main(String[] args) throws IOException, ParseException {
         Integer mode = 0;
+        String proxyHost = "";
+        String proxyPort = "";
+        String proxyUser = "";
+        String proxyPass = "";
         CommandLine commandLine;
         File file = null;
 
         Option option_RequestsFile = Option.builder("f").required(true).desc("Specify a File With Requests").hasArg().build();
         Option option_proxy = Option.builder("proxy").required(false).desc("Choose this option to use Proxy").build();
+        Option option_host = Option.builder("host").required(false).desc("Choose Proxy Host").hasArg().build();
+        Option option_port = Option.builder("port").required(false).desc("Choose Proxy Port").hasArg().build();
+        Option option_user = Option.builder("user").required(false).desc("Choose proxy User").hasArg().build();
+        Option option_pass = Option.builder("pass").required(false).desc("Choose Proxy Pass").hasArg().build();
         Option option_mode = Option.builder("m").required(true)
                 .desc("Select 1 for Resending Requests with ResponseChecks or 0 for no Checks").hasArg().build();
         Options options = new Options();
@@ -26,6 +40,10 @@ public class ReqReTest {
         options.addOption(option_RequestsFile);
         options.addOption(option_mode);
         options.addOption(option_proxy);
+        options.addOption(option_host);
+        options.addOption(option_port);
+        options.addOption(option_user);
+        options.addOption(option_pass);
 
         try {
             commandLine = parser.parse(options, args);
@@ -37,7 +55,19 @@ public class ReqReTest {
                 mode = Integer.parseInt(commandLine.getOptionValue("m"));
             }
             if (commandLine.hasOption("proxy")) {
-                System.out.println("Proxy Option is present Please Specify: --not Implemented \n");
+                System.out.println("proxy Not Implemented");
+                if (commandLine.hasOption("host")) {
+                    proxyHost = commandLine.getOptionValue("host");
+                }
+                if (commandLine.hasOption("port")) {
+                    proxyPort = commandLine.getOptionValue("port");
+                }
+                if (commandLine.hasOption("user")) {
+                    proxyUser = commandLine.getOptionValue("user");
+                if (commandLine.hasOption("pass")) {
+                    proxyPass = commandLine.getOptionValue("pass");
+                }
+                }
             }
 
         } catch (ParseException exception) {
